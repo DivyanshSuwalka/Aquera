2593ac1d972e43b04c0c1765988fe628482cf5b3
Divyansh Suwalka <76103075+DivyanshSuwalka@users.noreply.github.com>
2024-02-24 23:27:58 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,185 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import DataCard, { Card } from "@/components/card";
+import { Flex } from "antd";
+// import { Flex, Pagination, notification } from 'antd';
+import axios from "axios";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { TailSpin } from "react-loader-spinner";
+import ReactPaginate from "react-paginate";
 
-const inter = Inter({ subsets: ["latin"] });
+const Dashboard = (props: { data: any }) => {
+  const [data, setData] = useState(props?.data);
+  const [pageChange, setPageChange] = useState("");
+  const router = useRouter();
+  console.log(data, "fsafdsadfasdfsa");
+  const [itemOffset, setItemOffset] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-export default function Home() {
+  const endOffset = itemOffset + 10;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  // const currentItems = data.results.slice(itemOffset, endOffset);
+
+  const customRequest = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        pageChange || "https://swapi.dev/api/planets/?format=json"
+      );
+      setData(response.data);
+      console.log(response, "hfdajksfksjaf");
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    customRequest();
+  }, [pageChange]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      {loading ? (
+        <div
+          style={{
+            marginTop: "100px",
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TailSpin color="gray" radius={"8px"} />
         </div>
-      </div>
+      ) : (
+        <div>
+          <h1
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              textAlign: "center",
+              fontWeight: "800",
+              padding: "30px",
+              fontSize: "50px",
+              color: "#333333",
+            }}
+          >
+            Dashboard
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              // flexDirection: "column",
+              height: "auto",
+              flexWrap: "wrap",
+              width: "100%",
+              gap: "20px",
+              alignItems: "center",
+              justifyContent: "center",
+              // padding: "10px",
+              fontSize: "2rem",
+              alignContent: "center",
+            }}
+          >
+            {data.results?.map((value: Card, index: number) => {
+              return <DataCard key={index} data={value} />;
+            })}
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "10px",
+              margin: "40px",
+            }}
+          >
+            <div
+              onClick={() => {
+                setPageChange(data.previous);
+              }}
+              style={{
+                border: "1px solid #cccccc",
+                borderRadius: "6px",
+                padding: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Prev
+            </div>
+            <div style={{ display: "flex", gap: "10px" }}>
+              {(() => {
+                const arr = [];
+                for (let i = 0; i < data.count / 10; i++) {
+                  arr.push(
+                    <div
+                      onClick={() => {
+                        setTimeout(() => {
+                          router.push(`/dashboard?page=${i + 1}`);
+                        }, 1500);
+                        setPageChange(
+                          `https://swapi.dev/api/planets/?page=${
+                            i + 1
+                          }&format=json`
+                        );
+                      }}
+                      style={{
+                        border: "1px solid #cccccc",
+                        borderRadius: "6px",
+                        padding: "5px",
+                        cursor: "pointer",
+                      }}
+                    >
+                      <h1>{i + 1}</h1>
+                    </div>
+                  );
+                }
+                return arr;
+              })()}
+            </div>
+            <div
+              onClick={() => {
+                setPageChange(data.next);
+              }}
+              style={{
+                border: "1px solid #cccccc",
+                borderRadius: "6px",
+                padding: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Next
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  let data;
+  try {
+    const response = await axios.get(
+      "https://swapi.dev/api/planets/?format=json"
+    );
+    console.log(response, "hfdajksfksjaf");
+    data = response.data;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return {
+    props: {
+      data: data || null,
+    },
+  };
+};
+
+export default Dashboard;
